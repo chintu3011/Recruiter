fc8dea8f0e9da50cc63587845c83dc2335610f49
DarshShah1703 <darshtiger17@gmail.com>
2023-06-07 01:13:15 +0530

    fragment swapping issue solved

--- app/src/main/java/com/example/recruiter/HomeJobActivity.kt
+++ app/src/main/java/com/example/recruiter/HomeJobActivity.kt
@@ -1,11 +1,17 @@
 package com.example.recruiter
 
 import android.Manifest
+import android.app.AlertDialog
+import android.content.Intent
 //import android.content.Intent
 import android.content.pm.PackageManager
+import android.net.Uri
+import android.os.Build
 //import android.net.Uri
 //import android.os.Build
 import android.os.Bundle
+import android.provider.Settings
+import android.util.Log
 //import android.provider.Settings
 import android.view.View
 import android.view.Window
@@ -15,10 +21,18 @@ import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
 //import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContentProviderCompat.requireContext
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentTransaction
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.karumi.dexter.Dexter
+import com.karumi.dexter.MultiplePermissionsReport
+import com.karumi.dexter.PermissionToken
+import com.karumi.dexter.listener.multi.MultiplePermissionsListener
+
 //import com.karumi.dexter.Dexter
 //import com.karumi.dexter.MultiplePermissionsReport
 //import com.karumi.dexter.PermissionToken
@@ -52,22 +66,13 @@ class HomeJobActivity : AppCompatActivity() {
         window.statusBarColor = ContextCompat.getColor(this@HomeJobActivity,android.R.color.white)
         window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
 //        window.setBackgroundDrawable(background)
-
-
-        permissionLauncher = registerForActivityResult( ActivityResultContracts.RequestMultiplePermissions()){ permissions ->
-
-              isReadStorageGranted = permissions[Manifest.permission.READ_EXTERNAL_STORAGE] ?: isReadStorageGranted
-              isWriteStorageGranted = permissions[Manifest.permission.WRITE_EXTERNAL_STORAGE] ?: isWriteStorageGranted
-//              isCallPhoneGranted = permissions[Manifest.permission.CALL_PHONE] ?: isCallPhoneGranted
-//              isReceiveSmsGranted = permissions[Manifest.permission.RECEIVE_SMS] ?: isReceiveSmsGranted
-//              isSendSmsGranted = permissions[Manifest.permission.SEND_SMS] ?: isSendSmsGranted
-          }
-        requestPermissions()
-
+        if (!isGrantedPermission()) {
+            requestPermissions("bannerImg")
+        }
         bottomNavigationView = findViewById(R.id.bottomnavigation)
         frame = findViewById(R.id.frameLayout)
-//        replaceFragment(HomeFragment())
-        replaceFragment((ProfileFragment()))
+        replaceFragment(HomeFragment())
+//        replaceFragment((ProfileFragment()))
         homeFragment = HomeFragment()
         bottomNavigationView.setOnItemSelectedListener { item ->
             when (item.itemId) {
@@ -89,100 +94,99 @@ class HomeJobActivity : AppCompatActivity() {
             true
         }
     }
-
-    
-    private  fun requestPermissions(){
-        isReadStorageGranted = ContextCompat.checkSelfPermission(
-            this,
-            Manifest.permission.READ_EXTERNAL_STORAGE
-        ) == PackageManager.PERMISSION_GRANTED
-        isWriteStorageGranted = ContextCompat.checkSelfPermission(
-            this,
-            Manifest.permission.WRITE_EXTERNAL_STORAGE
-        ) == PackageManager.PERMISSION_GRANTED
-         val permissionRequest: MutableList<String> = ArrayList()
-
-
-        if (!isReadStorageGranted){
-            makeToast("not granted",0)
-            permissionRequest.add(Manifest.permission.READ_EXTERNAL_STORAGE)
-
-        }
-        if (!isWriteStorageGranted) permissionRequest.add(Manifest.permission.WRITE_EXTERNAL_STORAGE)
-
-        if (permissionRequest.isNotEmpty()){
-            makeToast("launched",0)
-            permissionLauncher.launch(permissionRequest.toTypedArray())
-        }
-
-    }
-
-//    private fun requestPermissions() {
-//
-//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
-//            Dexter.withContext(this)
-//
-//                .withPermissions(
-//
-//                    Manifest.permission.READ_MEDIA_IMAGES,
-//                    Manifest.permission.CALL_PHONE)
-//                .withListener(object : MultiplePermissionsListener {
-//                    override fun onPermissionsChecked(multiplePermissionsReport: MultiplePermissionsReport) {
-//                        if (multiplePermissionsReport.areAllPermissionsGranted()) {
-//                            Toast.makeText(this@HomeJobActivity, "All the permissions are granted..", Toast.LENGTH_SHORT).show()
-//                        }
-//
-//                        if (multiplePermissionsReport.isAnyPermissionPermanentlyDenied) {
-//
-//                        }
-//                    }
-//
-//                    override fun onPermissionRationaleShouldBeShown(list: List<PermissionRequest>, permissionToken: PermissionToken) {
-//
-//                        permissionToken.continuePermissionRequest()
-//                    }
-//                }).withErrorListener {
-//
-//                    Toast.makeText(applicationContext, "Error occurred! ", Toast.LENGTH_SHORT).show()
-//                }
-//
-//                .onSameThread().check()
-//        }
-//    }
-//
-//    private fun showSettingsDialog() {
-//
-//        val builder = AlertDialog.Builder(this@HomeJobActivity)
-//
-//
-//        builder.setTitle("Need Permissions")
-//
-//        builder.setMessage("This app needs permission to use this feature. You can grant them in app settings.")
-//        builder.setPositiveButton("GOTO SETTINGS") { dialog, which ->
-//
-//            dialog.cancel()
-//
-//            val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
-//            val uri = Uri.fromParts("package", packageName, null)
-//            intent.data = uri
-//            startActivityForResult(intent, 101)
-//        }
-//        builder.setNegativeButton("Cancel") { dialog, which ->
-//
-//            dialog.cancel()
-//        }
-//
-//        builder.show()
-//    }
-
-
-
     private fun replaceFragment(fragment: Fragment) {
         supportFragmentManager.beginTransaction()
             .replace(R.id.frameLayout, fragment)
             .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
             .commit()
     }
+
+    private fun requestPermissions(s: String) {
+        val permissions: Collection<String> =
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+                listOf(Manifest.permission.READ_MEDIA_IMAGES)
+            } else {
+                listOf(
+                    Manifest.permission.CAMERA,
+                    Manifest.permission.READ_EXTERNAL_STORAGE,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE
+                )
+            }
+        Log.d("####", "requestPermissions: $permissions")
+        Dexter.withContext(this).withPermissions(
+            permissions
+        ).withListener(object : MultiplePermissionsListener {
+            override fun onPermissionsChecked(report: MultiplePermissionsReport?) {
+
+                Log.d("####", "onPermissionsChecked: $report")
+                if (report?.areAllPermissionsGranted()!!) {
+                    Log.d("permissions###", "permission granted")
+                }
+                if (report.isAnyPermissionPermanentlyDenied) {
+                    // Show dialog when user denied permission permanently, show dialog message.
+                    Log.d("permission###", "permission Denied")
+                    showSettingsDialog()
+                }
+            }
+
+            override fun onPermissionRationaleShouldBeShown(
+                p0: MutableList<com.karumi.dexter.listener.PermissionRequest>?,
+                p1: PermissionToken?
+            ) {
+                p1?.continuePermissionRequest()
+
+            }
+        }).withErrorListener { error -> Log.e("#####", "onError $error") }.check()
+    }
+
+    private fun isGrantedPermission(): Boolean {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            Log.d("Version*", Build.VERSION.SDK_INT.toString())
+            listOf(Manifest.permission.CAMERA, Manifest.permission.READ_MEDIA_IMAGES)
+            val isGranted1 =
+                ContextCompat.checkSelfPermission(
+                    this,
+                    Manifest.permission.READ_MEDIA_IMAGES
+                )
+            val isGranted2 =
+                ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
+            return isGranted1 == PackageManager.PERMISSION_GRANTED && isGranted2 == PackageManager.PERMISSION_GRANTED
+        } else {
+            Log.d("Version**", Build.VERSION.SDK_INT.toString())
+            val isGranted1 =
+                ContextCompat.checkSelfPermission(
+                    this,
+                    Manifest.permission.READ_EXTERNAL_STORAGE
+                )
+            val isGranted2 =
+                ContextCompat.checkSelfPermission(
+                    this,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE
+                )
+            val isGranted3 =
+                ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
+
+            return isGranted1 == PackageManager.PERMISSION_GRANTED && isGranted2 == PackageManager.PERMISSION_GRANTED && isGranted3 == PackageManager.PERMISSION_GRANTED
+        }
+    }
+
+    private fun showSettingsDialog() {
+        val builder: AlertDialog.Builder = AlertDialog.Builder(this)
+        builder.setTitle("Need Permissions")
+        builder.setMessage("This app needs permission to use this feature. You can grant them in app settings.")
+        builder.setPositiveButton("GOTO SETTINGS") { dialog, which ->
+            dialog.cancel()
+            val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
+            val uri: Uri = Uri.fromParts("package", this.packageName, null)
+            intent.data = uri
+            startActivity(intent)
+        }
+        builder.setNegativeButton("Cancel") { dialog, which ->
+            dialog.cancel()
+        }
+        builder.show()
+    }
+
     private fun makeToast(msg: String, len: Int) {
         if (len == 0) Toast.makeText(this, msg, Toast.LENGTH_SHORT).show()
         if (len == 1) Toast.makeText(this, msg, Toast.LENGTH_LONG).show()
