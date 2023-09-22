763be82471babfecb4b06166fc4c0b00e87a5108
chintu3011 <cp899.cp@gmail.com>
2023-09-22 10:33:17 +0530

    In chat message notification are done

--- app/src/main/java/com/amri/emploihunt/authentication/OTPVerificationLoginActivity.kt
+++ app/src/main/java/com/amri/emploihunt/authentication/OTPVerificationLoginActivity.kt
@@ -158,7 +158,7 @@ class OTPVerificationLoginActivity : BaseActivity(),OnClickListener{
                         timer.start()
                         storedVerificationId = verificationId
                         resendToken = token
-                        makeToast("code sent to :${this@OTPVerificationLoginActivity.phoneNo}",0)
+                        makeToast("code sent to :${phoneNo}",0)
                         hideProgressDialog()
                     }
                 }
