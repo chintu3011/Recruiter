c48de96cffdbc06075ba1bacae69293cd3b76a67
chintu3011 <cp899.cp@gmail.com>
2023-10-23 17:17:35 +0530

    Final Change

--- app/src/main/java/com/amri/emploihunt/util/MainApp.kt
+++ app/src/main/java/com/amri/emploihunt/util/MainApp.kt
@@ -5,6 +5,10 @@ import android.os.StrictMode
 import android.os.StrictMode.VmPolicy
 import android.util.Log
 import com.androidnetworking.AndroidNetworking
+import com.google.firebase.appcheck.ktx.appCheck
+import com.google.firebase.appcheck.playintegrity.PlayIntegrityAppCheckProviderFactory
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.ktx.initialize
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
@@ -17,6 +21,11 @@ class MainApp : Application() {
         try {
             StrictMode.setVmPolicy(VmPolicy.Builder().build())
             AndroidNetworking.initialize(applicationContext)
+            Firebase.initialize(context = this)
+            Firebase.appCheck.installAppCheckProviderFactory(
+                PlayIntegrityAppCheckProviderFactory.getInstance(),
+            )
+
 
         } catch (e: Exception) {
             Log.e("MainApp", "MainApp onCreate Error: ${e.message}")
